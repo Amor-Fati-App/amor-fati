7514f1bc376cee0b524fd5a132cdc86ceda8ca75
Lauren Thorud <lthorud12@gmail.com>
2026-02-17 23:40:35 -0600

    update config

--- amor-fati/vite.config.js
+++ amor-fati/vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: {
+    port: 5173
+  },
   plugins: [react(), tailwindcss()],
 })
